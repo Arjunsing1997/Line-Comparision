9abd9c16f89eb1fd9126952a8703c03696ddac53
Arjunsing1997 <acchur1997@gmail.com>
2021-04-15 20:00:21 +0530

    UC1 calculating line length

--- Line-Comparision/Line-Comparision/Program.cs
+++ Line-Comparision/Line-Comparision/Program.cs
@@ -7,6 +7,26 @@ namespace Line_Comparision
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Line Comparision program");
+
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("Enter the value of X1: ");
+            int X1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter the value of Y1: ");
+            int Y1 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Enter the value of X2: ");
+            int X2 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter the value of Y2: ");
+            int Y2 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("X1:" + X1 + "\n" + "Y1" + Y1);
+            Console.WriteLine("X2:" + X2 + "\n" + "Y2" + Y2);
+            Console.WriteLine("X1:" + X1 + "\n" + "Y1" + Y1);
+            Console.WriteLine("X2:" + X2 + "\n" + "Y2" + Y2);
+
+            double lengthLine1 = Math.Sqrt((X2 - X1) ^ 2 + (Y2 - Y1) ^ 2);
+            Console.WriteLine("Lenth of the Line1: " + lengthLine1);
+
         }
     }
 }
